3c538ca909a710f8afca189c306ce17bb484a669
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-07-07 18:50:56 +0300

    Dot-env check

--- thank_project/thank_project/settings.py
+++ thank_project/thank_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+# load_dotenv()
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-pe)blv_17@!$4d_9+kbu2u*94lbjw-*or9uwpdv#+&ia9ittk#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
